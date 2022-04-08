f67f142715340ac1a5cec7ae15d198e83de1476c
Jobson Tenorio <jobsontenorio@gmail.com>
2022-04-08 15:15:43 -0300

    Algoritmo scilab até o fim

--- app/Http/Controllers/MainController.php
+++ app/Http/Controllers/MainController.php
@@ -33,7 +33,7 @@ class MainController extends Controller
     function ktkddiario($KT){
         $KD = 0;
         if(0.17 <= $KT && $KT <= 0.75){
-            $KD = (1.188-2.272*$KT+9.473*($KT)^2-21.856*($KT)^3+14.648*($KT)^4);
+            $KD = (1.188-2.272*$KT+9.473*($KT)**2-21.856*($KT)**3+14.648*($KT)**4);
         }
         if(0.75 < $KT && $KT <= 0.8){
             $KD = 0.632-0.54*$KT;
@@ -81,6 +81,41 @@ class MainController extends Controller
                         * $this->sind($Azimute)
                         * $this->sind($W);
         return $Costetas;
+    }
+
+    function Ktalehorario($KT,$KTM,$LAMBDA) {
+        $Ktale = 0;
+        if(0.10 <= $KT && $KT <= 0.75){
+            $Ktale= $KTM+$LAMBDA*(0.25-2*($KT-0.4)**2);
+        }
+        if(0.10> $KT || $KT >0.75){
+            $Ktale= $KTM;
+        }
+        if($KTM <=0){
+            $Ktale = 0;
+        }
+        return $Ktale;
+    }
+
+    function Fatortransposicaoliu($Betaa)
+    {
+        $Fr= (1 + $this->cosd($Betaa))/2;
+        return $Fr;
+    }
+
+    function idalee($Ialecorrigido,$ktalee)
+    {
+        $Idale = 0;
+        if($ktalee <=0.8 && $ktalee >=0.22){
+            $Idale= $Ialecorrigido*(0.9511-0.1604*$ktalee+4.388*$ktalee**2-16.638*$ktalee**3+12.336*$ktalee**4);
+        }
+        if($ktalee<0.22){
+            $Idale=$Ialecorrigido*(1-0.09*$ktalee);
+        }
+        if($ktalee>0.8){
+            $Idale= 0.165;
+        }
+        return $Idale;
     }
 
     /* ------------------------------------------------------------------------------------ */
@@ -226,9 +261,113 @@ class MainController extends Controller
             }
         }
 
-        //dd($declinacao, $h0, $KT, $latitude1);
-        dd($rr);
+        $lambda = [
+            [-0.7011799,-0.3596322,-0.5947891,-0.1002283,0.5415149,-0.5129552,-0.574777,-0.7801532,0.3962962,-0.1698187,0.0059638,0.5023214,0.9880294,-0.6342475,-0.3956165,-0.2429027],
+			[0.4306397,0.9048307,-0.0592163,-0.6258117,-0.4885624,-0.1129868,0.4468156,0.752382,-0.9253358,-0.1413067,-0.3685534,-0.2635045,-0.7082451,0.3536759,0.0523959,-0.1992749],
+			[-0.9941784,-0.3863637,0.5805388,0.9155901,0.3378542,-0.4140768,0.6447799,-0.9640309,0.7421403,-0.3637951,0.1448947,0.1477316,-0.3015964,0.4134595,-0.2801599,-0.1895377],
+			[0.2281621,0.3048094,-0.9409658,0.1337792,0.4228034,-0.0823019,-0.0558965,-0.8752537,-0.8291198,-0.9730872,-0.2913996,0.3438789,-0.7278762,-0.5760513,-0.1968115,-0.1927563],
+			[0.1256764,0.1062186,-0.8462032,0.0721517,-0.8199576,0.2436051,-0.999757,0.4439455,0.3322585,0.3598577,0.902825,-0.7586197,0.463782,-0.8336676,0.6043817,-0.9419017],
+			[0.3005983,0.4855764,-0.5646778,0.9576973,0.7850474,-0.297745,-0.0508763,0.5398924,-0.8531315,0.1789237,-0.5242014,-0.1847278,-0.1390363,-0.9258566,0.2808297,-0.1738514],
+			[0.6793805,-0.9995611,0.0170748,-0.0490005,0.9250131,0.9596023,-0.8428603,0.8300064,0.234522,0.9928389,-0.9062803,0.8491852,0.937973,-0.0167421,-0.4045894,-0.8793891],
+			[0.2626944,-0.9517939,-0.6945123,0.8962355,-0.4511469,-0.0410546,-0.0288463,0.3528007,0.66425,-0.9748248,0.090756,0.6853434,0.805833,-0.1181036,0.6664718,0.4467953],
+			[-0.12457,-0.3838785,0.7499626,0.0711765,-0.3828002,-0.3290735,-0.5315027,-0.4821177,0.7043018,-0.0356521,0.2190436,0.9745645,0.9622854,-0.3393773,-0.2821709,-0.4438945],
+			[0.9166964,-0.9687274,-0.4071297,-0.062813,-0.1475958,-0.1564687,-0.7930293,-0.1440481,0.5721459,0.7136164,-0.6013125,0.4262603,0.0417905,0.8623446,-0.1712328,0.1960392],
+			[0.1098211,0.7105905,-0.3804499,0.8892256,-0.5114639,0.7520894,-0.0251569,-0.2311962,-0.8155309,-0.8589163,0.4677615,0.5584361,0.5655276,-0.5676864,-0.3159604,-0.1398123],
+			[0.2523505,0.9108502,-0.0916899,-0.9941554,-0.9786159,0.9492742,0.5408522,0.2793263,0.9071772,-0.1703033,-0.0676921,-0.552309,-0.3086053,-0.5318753,0.3238683,-0.4617641]
+        ];
 
+        for ($i = 0; $i < 12; $i++){
+            //kd(i) = ktkddiario(KT(i));
+            $kd[$i] = $this->ktkddiario($KT[$i]);
+            //hd(i) = kd(i)*Hm(i)
+            $hd[$i] = $kd[$i] * $Hm[$i];
+            //hb(i) = Hm(i)-hd(i)
+            $hb[$i] = $Hm[$i] - $hd[$i];
+            //a(i)= 0.409+0.5016*sind(ws(i)-60)
+            $a[$i] = 0.409 + 0.5016 * $this->sind($ws[$i] - 60);
+            //b(i)= 0.6609-0.4767*sind(ws(i)-60)
+            $b[$i] = 0.6609 - 0.4767 *  $this->sind($ws[$i] - 60);
+            for ($j = 0; $j < count($HS); $j++){
+                //w(j) = 15*(HS(j)-12)
+                $w[$j] = 15 * ($HS[$j] - 12);
+                // I(i,j) = (Hm(i)*(%pi*(a(i)+b(i)*cosd(w(j)))*(cosd(w(j))-cosd(ws(i)))/(24*(sind(ws(i))-(ws(i)*%pi/180)*cosd(ws(i))))));
+                $I[$i][$j] = ($Hm[$i] * (pi() * ($a[$i] + $b[$i] * $this->cosd($w[$j])) * ($this->cosd($w[$j]) - $this->cosd($ws[$i])) / (24 * ($this->sind($ws[$i]) - ($ws[$i] * pi() / 180) * $this->cosd($ws[$i])))));
+                /* if I(i,j) < 0 then
+                    I(i,j) = 0;
+                end */
+                if($I[$i][$j] < 0){
+                    $I[$i][$j] = 0;
+                }
+
+                //lambda(i,j) =(rand()*(1+1)-1) //-1 até 1
+                $lambda[$i][$j] = mt_rand (-100000, 100000) / 100000; // -1 até 1 
+
+                //ktm(i,j) = I(i,j)/(i0efetivo(i,j)*costetaz(i,j));//kt horário não aleatório
+                $ktm[$i][$j] = $I[$i][$j] / ( $i0efetivo[$i][$j] * $costetaz[$i][$j]); //kt horário não aleatório
+                //ktale(i,j) = Ktalehorario(KT(i),ktm(i,j),lambda(i,j));
+                $ktale[$i][$j] = $this->Ktalehorario($KT[$i],$ktm[$i][$j],$lambda[$i][$j]);
+               /*  if ktm(i,j)>0 then  //kt horário
+                    Iale(i,j) = ktale(i,j)*i0efetivo(i,j)*costetaz(i,j)
+                else
+                    Iale(i,j)=0
+                end */
+                if($ktm[$i][$j] > 0){
+                    $Iale[$i][$j] = $ktale[$i][$j] * $i0efetivo[$i][$j] * $costetaz[$i][$j];
+                } else {
+                    $Iale[$i][$j] = 0;
+                }
+            }
+        }
+        dd($lambda);
+
+        $sum = function ($carry, $item) {
+            $carry += $item;
+            return $carry;
+        };
+
+        for ($i = 0; $i < 12; $i++){
+            for ($j = 0; $j < count($HS); $j++){
+                //Ialecorrigido(i,j)= (Iale(i,j)*Hm(i))/sum(Iale(i,:))      // Dando erro fazendo o mesmo valor de H apenas.
+                $Ialecorrigido[$i][$j] = ($Iale[$i][$j] * $Hm[$i]) / array_reduce($Iale[$i], $sum);      // Dando erro fazendo o mesmo valor de H apenas.
+                /* if Ialecorrigido(i,j)<0 then
+                    Ialecorrigido(i,j)= 0
+                end */
+                if($Ialecorrigido[$i][$j] < 0){
+                    $Ialecorrigido[$i][$j] = 0;
+                }
+
+                //idale(i,j) = idalee(Ialecorrigido(i,j),ktale(i,j));
+                $idale[$i][$j] = $this->idalee($Ialecorrigido[$i][$j], $ktale[$i][$j]);
+
+                //restante dos cálculos.
+
+                //ibale(i,j)= Ialecorrigido(i,j)-idale(i,j)
+                $ibale[$i][$j] = $Ialecorrigido[$i][$j] - $idale[$i][$j];
+                //ibtale(i,j)= ibale(i,j)*(costetas(i,j)/costetaz(i,j))
+                $ibtale[$i][$j] = $ibale[$i][$j] * ($costetas[$i][$j] / $costetaz[$i][$j]);
+                //fr(i,j) = Fatortransposicaoliu(betaa)  //Fator de transposição para plano inclinado Liu Jordan
+                $fr[$i][$j] = $this->Fatortransposicaoliu($inclinacao);  //Fator de transposição para plano inclinado Liu Jordan
+                //idtale(i,j)= idale(i,j)*fr(i,j)
+                $idtale[$i][$j] = $idale[$i][$j] * $fr[$i][$j];
+                //pale(i,j) = rr(i,j)*Ialecorrigido(i,j)*reflexao
+                $pale[$i][$j] = $rr[$i][$j] * $Ialecorrigido[$i][$j] * $reflexao;
+                //Ghtale(i,j)= pale(i,j) + idtale(i,j) + ibtale(i,j)
+                $Ghtale[$i][$j] = $pale[$i][$j] + $idtale[$i][$j] + $ibtale[$i][$j];
+            }
+            //diario(i) = sum(Ghtale(i,:));
+            $diario[$i] = array_reduce($Ghtale[$i], $sum);
+        }
+
+        //dd($fr);
+
+        //mediadiariahorizontal = mean(Hm);
+        $mediadiariahorizontal = array_sum($Hm)/count($Hm);
+        //mediadiaria = mean(diario);
+        $mediadiaria = array_sum($diario)/count($diario);
+
+
+        //dd($declinacao, $h0, $KT, $latitude1);
+        dd($mediadiariahorizontal, $mediadiaria);
 
 
         //fim da área das constantes
@@ -236,37 +375,4 @@ class MainController extends Controller
         return 10;
     }
 
-}
-
-function Fatortransposicaoliu($Betaa)
-{
-    $Fr= (1 + cos($Betaa))/2;
-    return $Fr;
-}
-function Ktalehorario($KT,$KTM,$LAMBDA)
-{
-    $Ktale = 0;
-    if(0.10 <= $KT && $KT <= 0.75){
-        $Ktale= $KTM+$LAMBDA*(0.25-2*($KT-0.4)^2);
-    }
-    if(0.10> $KT || $KT >0.75){
-        $Ktale= $KTM;
-    }
-    if($KTM <=0){
-        $Ktale = 0;
-    }
-}
-function idalee($Ialecorrigido,$ktalee)
-{
-    $Idale = 0;
-    if($ktalee <=0.8 && $ktalee >=0.22){
-        $Idale= $Ialecorrigido*(0.9511-0.1604*$ktalee+4.388*$ktalee^2-16.638*$ktalee^3+12.336*$ktalee^4);
-    }
-    if($ktalee<0.22){
-        $Idale=$Ialecorrigido*(1-0.09*$ktalee);
-    }
-    if($ktalee>0.8){
-        $Idale= 0.165;
-    }
-    return $Idale;
 }
