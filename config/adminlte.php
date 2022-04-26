54427fc63850f8472d2c6465490270c685153004
Jobson Tenorio <jobsontenorio@gmail.com>
2022-04-26 09:15:51 -0300

    correção da url / no adminLTE

--- config/adminlte.php
+++ config/adminlte.php
@@ -189,7 +189,7 @@ return [
     */
 
     'use_route_url' => false,
-    'dashboard_url' => 'ferramenta1_form_admin',
+    'dashboard_url' => '/',
     'logout_url' => 'logout',
     'login_url' => 'login',
     'register_url' => 'register',
